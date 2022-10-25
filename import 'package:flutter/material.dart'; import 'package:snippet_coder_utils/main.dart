d663e30561d65ae9d46e24c2429507c58854fe8c
AkYazilimDestek <111167457+AkYazilimDestek@users.noreply.github.com>
2022-10-25 05:19:14 +0300

    update main.dart

--- import 'package:flutter/material.dart'; import 'package:snippet_coder_utils/main.dart
+++ import 'package:flutter/material.dart'; import 'package:snippet_coder_utils/main.dart
@@ -10,103 +10,104 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home : Scaffold(
-        body : _Iskele()
-      )
+      home : _Vucut(),
     );
   }
 }
-
-
-class _Iskele extends StatefulWidget {
-  const _Iskele({Key? key}) : super(key: key);
+class _Vucut extends StatefulWidget {
+  const _Vucut({Key? key}) : super(key: key);
 
   @override
-  State<_Iskele> createState() => _IskeleState();
+  State<_Vucut> createState() => _VucutState();
 }
 
-class _IskeleState extends State<_Iskele> {
-  List<dynamic> countries = [];
-  List<dynamic> statesMastes = [];
-  List<dynamic> states = [];
+class _VucutState extends State<_Vucut> {
 
+  String UrunKat = "";
+  List<dynamic> UrunKategoriler = [
+    {"UrunKat":1,"KatAdi":"KAT 1"},
+    {"UrunKat":2,"KatAdi":"KAT 2"},
+    {"UrunKat":3,"KatAdi":"KAT 3"},
+    {"UrunKat":4,"KatAdi":"KAT 4"},
+  ];
 
-  String? countryId;
-  String? stateId;
+  String Urun = "";
+  List<dynamic> UrunListesi = [
+    {"UrunID":1,"UrunAdi":"ÜRÜN 1","Kat":1},
+    {"UrunID":2,"UrunAdi":"ÜRÜN 2","Kat":1},
+    {"UrunID":3,"UrunAdi":"ÜRÜN 3","Kat":1},
+    {"UrunID":4,"UrunAdi":"ÜRÜN 4","Kat":2},
+    {"UrunID":5,"UrunAdi":"ÜRÜN 5","Kat":2},
+    {"UrunID":6,"UrunAdi":"ÜRÜN 6","Kat":2},
+    {"UrunID":7,"UrunAdi":"ÜRÜN 7","Kat":3},
+    {"UrunID":8,"UrunAdi":"ÜRÜN 8","Kat":3},
+    {"UrunID":9,"UrunAdi":"ÜRÜN 9","Kat":3},
+    {"UrunID":10,"UrunAdi":"ÜRÜN 10","Kat":3},
+    {"UrunID":11,"UrunAdi":"ÜRÜN 11","Kat":4},
+    {"UrunID":12,"UrunAdi":"ÜRÜN 12","Kat":4},
+    {"UrunID":13,"UrunAdi":"ÜRÜN 13","Kat":4},
+    {"UrunID":14,"UrunAdi":"ÜRÜN 14","Kat":4},
+  ];
 
-  void initState(){
-    super.initState();
-    this.countries.add({"val": 1,"label":"KAYSERİ"});
-    this.countries.add({"val": 2,"label":"ADANA"});
-    this.statesMastes = [
-      {"ID" : 1,"Name":"ilçe KAYSERİ-1","parentId":1},
-      {"ID" : 2,"Name":"ilçe KAYSERİ-2","parentId":1},
-      {"ID" : 3,"Name":"ilçe KAYSERİ-3","parentId":1},
-      {"ID" : 4,"Name":"ilçe ADANA-1","parentId":2},
-      {"ID" : 5,"Name":"ilçe ADANA-2","parentId":2},
-      {"ID" : 6,"Name":"ilçe ADANA-3","parentId":2},
-      {"ID" : 7,"Name":"ilçe ADANA-4","parentId":2},
-    ];
-  }
+  List<dynamic> UrunSecim = [];
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("AĞAĞIDAKİ CANLILARDAN BİRİNİ SEÇİNİZ"),
-        ),
-        body: Column(
-          children: [
-            FormHelper.dropDownWidgetWithLabel(
-                context, "SELECT COUNTRY",
-                "İL",
-                this.countryId, this.countries,
-                (onChangedVal){
-                  this.countryId = onChangedVal;
-                  print("elected Country : $onChangedVal");
-                  setState((){
 
-                    this.states = this.statesMastes.where(
-                            (stateItem) => stateItem["parentId"].toString() == onChangedVal.toString()
-                    ).toList();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ÜST APP BAR"),
+      ),
+      body : Column(
+        children: [
 
-                  });
-                  this.stateId = null;
-                },
+
+          FormHelper.dropDownWidgetWithLabel(
+            context,
+            "ÜRÜN KAT",
+            "",
+            this.UrunKat,
+            this.UrunKategoriler,
+                (onChangedVal) {
+              setState(() {
+                this.UrunKat = onChangedVal;
+                this.UrunSecim = this.UrunListesi.where((element) => element["Kat"].toString() == onChangedVal.toString()).toList();
+              });
+            },
                 (onValidateVal) {
-                  if (onValidateVal == null){
-                    return 'Please Select Country';
-                  }
-                  return null;
-                },
-                borderColor : Theme.of(context).primaryColor,
-                borderFocusColor:  Theme.of(context).primaryColor,
-                borderRadius: 10,
-                optionValue: "val",
-                optionLabel: "label",
-            ),
-            FormHelper.dropDownWidgetWithLabel(
-                context,
-                "State",
-                "Select State",
-                this.stateId,
-                this.states,
-                (onChangedVal){
-                  this.stateId = onChangedVal;
-                  print("Select state : $onChangedVal");
-                },
-            (onValidate) {
-                return null;
+              return null;
             },
             borderColor: Theme.of(context).primaryColor,
             borderFocusColor: Theme.of(context).primaryColor,
+            optionValue: "UrunKat",
+            optionLabel: "KatAdi",
+          ),
+
+
+          FormHelper.dropDownWidgetWithLabel(
+            context,
+            "BAŞLIK",
+            "PLACEHOLDER",
+            this.Urun,
+            this.UrunSecim,
+                (onChangedVal){
+              setState(() {
+                this.Urun = onChangedVal;
+              });
+            },
+                (onChangedVal){
+              return null;
+            },
             borderRadius: 10,
-            optionValue: "ID",
-            optionLabel: "Name",
-            ),
-          ],
-        ),
+            borderFocusColor: Theme.of(context).primaryColor,
+            borderColor: Theme.of(context).primaryColor,
+            optionLabel: "UrunAdi",
+            optionValue: "UrunID",
+          ),
+
+
+
+        ],
       ),
     );
   }
